f85760aa679a4dc692c5f1efb132d8e9894d3801
ltree <ltree930@163.com>
2026-02-03 09:51:51 +0800

    add array.h

---
+++ array.h
@@ -0,0 +1,24 @@
+#ifndef _ARRY_H_
+#define _ARRY_H_
+
+/**
+ * Static array copy method
+ * - dest: destination array for copy
+ * - src : source array to copy.
+ * - size: size to copy (must be a numeric literal)
+*/
+#define _static_arrycopy(dest, src, size)
+  do {                                                                         \
+    typedef struct {                                                           \
+      uint16_t _[size];                                                        \
+    } * array_p;                                                               \
+    *((array_p)dest) = *((array_p)src);                                        \
+  } while (0)
+
+/**
+ * Gets the size of array members of different types.
+ * - array destination array.
+*/
+#define _asizeof(array) (sizeof(array) / sizeof(array[0]))
+
+#endif
